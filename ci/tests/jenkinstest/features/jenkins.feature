0d20dfff2f0f20a8377c9f4e8bb1e20dbc554593
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-04-11 07:21:26 +0000

    Added a test to ensure anon user cannot create jobs

--- ci/tests/jenkinstest/features/jenkins.feature
+++ ci/tests/jenkinstest/features/jenkins.feature
@@ -1,4 +1,4 @@
-Feature: jenkins configuration
+Feature: jenkins
   In order have a valid jenkins installation
   As an administrator
   I need to be able to login to jenkins
@@ -7,3 +7,8 @@ Feature: jenkins configuration
     Given there is a jenkins install
     When I access the login screen
     Then jenkins should be unlocked
+
+  Scenario: Anonymous user cannot create jobs
+    Given there is a jenkins install
+	When an anonymous user tries to create a job
+	Then the user is prompted to login
--- ci/tests/jenkinstest/godogs_test.go
+++ ci/tests/jenkinstest/godogs_test.go
@@ -1,36 +1,44 @@
 package main
 
 import (
-	"strings"
 	"fmt"
-	"os"
-	"net/http"
-	"io/ioutil"
 	"github.com/DATA-DOG/godog"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 )
 
 var url string
 var body string
 
-func thereIsAJenkinsInstall() error {
-	url = os.Getenv("JENKINS_HOST") + "/login"
-	return nil
-}
-
-func iAccessTheLoginScreen() error {
+func page(page string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defer resp.Body.Close()
 	body_bytes, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	body = string(body_bytes)
+	return string(body_bytes), nil
+}
+
+func thereIsAJenkinsInstall() error {
+	url = os.Getenv("JENKINS_HOST")
+	return nil
+}
+
+func iAccessTheLoginScreen() error {
+	page_body, err := page("/login")
+	if err != nil {
+		return err
+	}
+	body = page_body
 	return nil
 }
 
@@ -41,9 +49,26 @@ func jenkinsShouldBeUnlocked() error {
 	return nil
 }
 
+func anAnonymousUserTriesToCreateAJob() error {
+	page_body, err := page("/newJob")
+	if err != nil {
+		return err
+	}
+	body = page_body
+	return nil
+}
+
+func theUserIsPromptedToLogin() error {
+	if !strings.Contains(body, "<a href=\"/login?from=/\">Log in</a> to create new jobs.") {
+		return fmt.Errorf("expected %s to contain '<a href=\"/login?from=/\">Log in</a> to create new jobs.'", body)
+	}
+	return nil
+}
+
 func FeatureContext(s *godog.Suite) {
 	s.Step(`^there is a jenkins install$`, thereIsAJenkinsInstall)
 	s.Step(`^I access the login screen$`, iAccessTheLoginScreen)
 	s.Step(`^jenkins should be unlocked$`, jenkinsShouldBeUnlocked)
+	s.Step(`^an anonymous user tries to create a job$`, anAnonymousUserTriesToCreateAJob)
+	s.Step(`^the user is prompted to login$`, theUserIsPromptedToLogin)
 }
-
